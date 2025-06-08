d55a054296fc4d247fbe116a1533609e783be701
Taehoon Moon <taehoon.moon@outlook.com>
2025-06-08 22:14:44 +0900

    Simplify TODO list parsing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 export default {
-  async fetch(_request, _env, _ctx) {
+  async fetch(request, env, _ctx) {
+    const url = new URL(request.url);
+
+    if (url.pathname === "/add") {
+      const content = url.searchParams.get("content");
+      if (!content) {
+        return new Response("Missing content", { status: 400 });
+      }
+
+      const rawList = await env.TODOS.get("list");
+      const list = rawList ? JSON.parse(rawList) : [];
+
+      list.push({ content });
+      await env.TODOS.put("list", JSON.stringify(list));
+
+      return new Response(JSON.stringify(list), {
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
     return new Response("Hello World!");
   },
 };
